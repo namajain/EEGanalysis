f2b54e908819b3b9d8ca87fbcc00dcbd79f37c3f
namajain <nammanjain@gmail.com>
2018-11-17 13:20:33 +0530

    added labels

--- FFT.py
+++ FFT.py
@@ -12,8 +12,9 @@ MEDITATING_CSV = 'meditating.csv'
 
 FREQ = 256
 
+
 def plotFFT(csv):
-    df =prepareEEGdata(csv)
+    df = prepareEEGdata(csv)
     # df.plot()
     # remove outliers
     # df=df[np.abs(df.fp2-df.fp2.mean()) <= (100*df.fp2.std())]
@@ -29,7 +30,11 @@ def plotFFT(csv):
     # ymin = 0
     # ymax = 20000
     # axes.set_ylim([ymin,ymax])
+    plt.xlabel("Frequency")
+    plt.ylabel("Amplitude")
+    plt.title("FFT")
     plt.show()
+
 
 def prepareEEGdata(csv):
     df = pandas.read_csv(csv)
@@ -38,27 +43,26 @@ def prepareEEGdata(csv):
     df = df[~df.fp2.str.contains("FP")]
     df = df.astype(float)
 
-    df.fp2 = df.fp2.clip(lower =CLIPDOWN, upper =CLIPUP)
+    df.fp2 = df.fp2.clip(lower=CLIPDOWN, upper=CLIPUP)
 
     df['diff1'] = df.fp2.diff()
     df['diff2'] = df.diff1.diff()
     df = df.dropna()
-    print(df)
     df = convertToDateIndexFromNum(df)
-    print(df)
 
     return df
 
-def plotRange(df,dfCol,start,end):
 
-    df.iloc[:df.fp2.size // 10].fp2.plot()
-    # df.tail(1000).fp2.plot()
-    plt.show()
+# def plotRange(df,dfCol,start,end):
+#
+#     df.iloc[:df.fp2.size // 10].fp2.plot()
+#     # df.tail(1000).fp2.plot()
+#     plt.show()
 
 
 def convertToDateIndexFromNum(df):
     df = df.reset_index(drop=True)
-    df['id']=df.index
+    df['id'] = df.index
     df.index = df.index / FREQ
     df.index = pandas.to_datetime(df.index, unit='s')
     return df
@@ -68,16 +72,18 @@ def showHist(df):
     df.hist(column='fp2', bins=1000)
     plt.show()
 
-def resampleData(df,timeFrame, show):
-    resampledData= pandas.DataFrame()
+
+def resampleData(df, timeFrame, show):
+    resampledData = pandas.DataFrame()
     resampledData['fp2'] = df.fp2.resample(timeFrame).mean()
-    if (show):
+    if show:
         resampledData.plot()
         plt.show()
     return resampledData
 
-df = prepareEEGdata(MEDITATING_CSV)
-showHist(df)
+
+dfMed = prepareEEGdata(MEDITATING_CSV)
+showHist(dfMed)
 
 # plotFFT(MEDITATING_CSV)
 # plotFFT(THINKING_CSV)
